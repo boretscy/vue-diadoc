979fd0e6e01b0675001fb556529c5cbf4efee630
boretscy <boretscy@gmail.com>
2021-12-28 11:10:40 +0300

    q

--- src/main.js
+++ src/main.js
@@ -1,36 +1,13 @@
-// let DiadocUser = User || null;
-
 import Vue from 'vue';
 import Vuex from 'vuex';
-// import Axios from 'axios';
 
 import App from './App.vue';
 
-// Vue.use(Vuex);
-
-// Axios("https://portal.yug-avto.ru/service/diadoc/api/?user="+DiadocUser)
-//     .then(response => {
-//         console.log(response.data);
-        
-//     })
-//     .catch(error => { console.log(error) });
-
-
-    const DiadocStore = new Vuex.Store({
-        state: Start
-    });
-    new Vue({
-        render: h => h(App),
-        store: DiadocStore,
-    })
-    .$mount('#diadoc');
-
-
-// new Vue({
-//     render: h => h(App),
-//     data: function { return Start },
-//     mounted: function() {
-
-//         console.log(this.Data);
-//     }
-// })
+const DiadocStore = new Vuex.Store({
+    state: DiadocStart
+});
+new Vue({
+    render: h => h(App),
+    store: DiadocStore,
+})
+.$mount('#diadoc');
